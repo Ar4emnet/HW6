803d5757573dbefd6b61605e9ab3c771e773b290
Artem <artemden.kolesnik@gmail.com>
2023-09-26 23:19:38 +0300

    did HW6.6

--- main.py
+++ main.py
@@ -29,6 +29,15 @@ def remove_number(list1):
     return "the number of deleted number "+str(res)
 def plus_list(list1,list2):
     return list1+list2
+def power_number(list1,):
+    res = []
+    numb = int(input("Write power for number "))
+    for number in list1:
+        number1 = number
+        for i in range(numb-1):
+            number1 *= number
+        res.append(number1)
+    return res
 list1 = []
 list2 = []
 for i in range(10):
@@ -45,3 +54,4 @@ print("list after remove number", end=" ")
 print(list1)
 print("list1+list2", end=" ")
 print(plus_list(list1, list2))
+print(power_number(list1))
