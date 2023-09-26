fe2aa869f0c2ea1fdc2c6835d4a6fec9b23ce2ce
Artem <artemden.kolesnik@gmail.com>
2023-09-26 23:16:16 +0300

    did HW6.3

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ def min_list(list1):
         if res > number:
             res = number
     return res
+
+def find_prime_number(list1):
+    res = 0
+    for number in list1:
+        for i in range(number)[2:]:
+            if (number % i) == 0:
+                res += 1
+    return res
 list1 = []
 for i in range(10):
     list1.append(randint(-100,100))
 print(list1)
 print("product list ",product_list(list1))
 print("min list ",min_list(list1))
+print("find primery numbers ",find_prime_number(list1))
