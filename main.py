b05e970715cc5301e5817676667006e09e96b502
Artem <artemden.kolesnik@gmail.com>
2023-09-26 23:14:02 +0300

    did HW6.2

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ def product_list(list1):
     for number in list1:
         res *= number
     return res
+
+def min_list(list1):
+    res = list1[0]
+    for number in list1[1:]:
+        if res > number:
+            res = number
+    return res
 list1 = []
 for i in range(10):
     list1.append(randint(-100,100))
 print(list1)
 print("product list ",product_list(list1))
+print("min list ",min_list(list1))
