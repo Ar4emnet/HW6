df1bfafc7ace9fb0d1ada9aeaa8b6b2f966e4b81
Artem <artemden.kolesnik@gmail.com>
2023-09-26 23:13:01 +0300

    did HW6.1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from random import randint
 
+def product_list(list1):
+    res = 1
+    for number in list1:
+        res *= number
+    return res
+list1 = []
+for i in range(10):
+    list1.append(randint(-100,100))
+print(list1)
+print("product list ",product_list(list1))
